958037a89cc13a62de226066747a5b1ff3ea849e
Hajara-Yasmin Isa <hisa2@uic.edu>
2021-02-07 15:58:43 -0600

    Updated write up and images

--- sketch.js
+++ sketch.js
@@ -1,3 +1,24 @@
+// **********************************************************************************
+// NAME:     HAJARA-YASMIN ISA
+// DATE:     FEBUARY 7 2021
+// FILE:     PROJECT 1 GENERATIVE DESIGN
+// COURSE:   ART 151 UIC
+// **********************************************************************************
+//
+// ***********************************************************************************
+//                             Write up portion
+//                      -----------------------------
+//  For this project my generative art focuses on the backround. The user
+//  can use their mouse to control how the draw is created my either moving
+//  their cursor up or down the screen or left or right. The fading background
+//  ultimately becomes the final product. I also use the concept of symmetry.
+//  The symmetry I used is embedded in the rings that make up the drawings. 
+//  As the user moves their mouse to create different types of ring configurations
+//  all the rings on the screen are also converted to the same configuration. 
+//  I have also included a directory with screenshots of some of the different images
+//  a user can create with my generative art program.  
+// ************************************************************************************
+
 let t = 0;
 let pos = 0;
 
@@ -11,13 +32,13 @@ function setup() {
   }
   
   function draw() {
-    background(25,104, 154);
+    background(25,1);
 
     
     //translate(350,350);
     for(let x = 0; x <= width; x +=30){
 
-      // rotate(radians(frameCount/2));
+      rotate(radians(frameCount/24));
        //fill(30, 15);
        //rect(0,0,x,x);
 
@@ -28,7 +49,7 @@ function setup() {
         const angle = xAngle * (x/width) + yAngle *(y/height);
         const myX = x + 40 * cos(2 * PI * t + angle);
         const myY = x + 35 * sin(2 * PI * t + angle);
-       // fill(random(255), random(255), random(255));
+       //fill(random(255), random(255), random(255));
         ellipse(myX, myY, 10);
 
       }
